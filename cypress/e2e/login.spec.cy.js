8a430e527e29a10bba649c94dd0b80fa1edee392
olivasilva300-cell <olivasilva300@gmail.com>
2026-05-12 21:43:49 -0300

    base url added

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -13,7 +13,7 @@ describe('Orange HRM Tests', () => {
 
   it('Login-Sucess', () => {
 
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
     cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
     cy.get(selectorsList.loginButton).click()
